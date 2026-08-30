0818262ffe8eac62b9dacf96ab9268181c112b1c
siddharthkollon <siddharthkollon@gmail.com>
2026-08-30 23:01:53 +0530

    Update generated game

--- Game.py
+++ Game.py
@@ -1,76 +1,65 @@
 import streamlit as st
 from streamlit_autorefresh import st_autorefresh
 import random
+
 st_autorefresh(interval=200, key="game_loop_ticker")
+
+if 'height' not in st.session_state:
+    st.session_state.height = 15
+if 'width' not in st.session_state:
+    st.session_state.width = 30
+if 'paddle_height' not in st.session_state:
+    st.session_state.paddle_height = 3
+if 'left_paddle_y' not in st.session_state:
+    st.session_state.left_paddle_y = (st.session_state.height - st.session_state.paddle_height) // 2
+if 'right_paddle_y' not in st.session_state:
+    st.session_state.right_paddle_y = (st.session_state.height - st.session_state.paddle_height) // 2
 if 'ball_x' not in st.session_state:
-    st.session_state.ball_x = 15
+    st.session_state.ball_x = st.session_state.width // 2
 if 'ball_y' not in st.session_state:
-    st.session_state.ball_y = 7
+    st.session_state.ball_y = st.session_state.height // 2
 if 'ball_dx' not in st.session_state:
-    st.session_state.ball_dx = 1
+    st.session_state.ball_dx = random.choice([-1, 1])
 if 'ball_dy' not in st.session_state:
-    st.session_state.ball_dy = 1
-if 'paddle_left_y' not in st.session_state:
-    st.session_state.paddle_left_y = 6
-if 'paddle_right_y' not in st.session_state:
-    st.session_state.paddle_right_y = 6
-if 'score_left' not in st.session_state:
-    st.session_state.score_left = 0
-if 'score_right' not in st.session_state:
-    st.session_state.score_right = 0
-if 'game_over' not in st.session_state:
-    st.session_state.game_over = False
-if 'winner' not in st.session_state:
-    st.session_state.winner = ""
-width = 30
-height = 15
-paddle_size = 4
-if not st.session_state.game_over:
-    st.session_state.ball_x += st.session_state.ball_dx
-    st.session_state.ball_y += st.session_state.ball_dy
-    if st.session_state.ball_y <= 0 or st.session_state.ball_y >= height - 1:
-        st.session_state.ball_dy *= -1
-    if st.session_state.ball_x <= 1:
-        if st.session_state.paddle_left_y <= st.session_state.ball_y < st.session_state.paddle_left_y + paddle_size:
-            st.session_state.ball_dx *= -1
-            st.session_state.ball_x = 1
-        else:
-            st.session_state.score_right += 1
-            st.session_state.ball_x = width // 2
-            st.session_state.ball_y = height // 2
-            st.session_state.ball_dx = 1
-            st.session_state.ball_dy = random.choice([-1, 1])
-    if st.session_state.ball_x >= width - 2:
-        if st.session_state.paddle_right_y <= st.session_state.ball_y < st.session_state.paddle_right_y + paddle_size:
-            st.session_state.ball_dx *= -1
-            st.session_state.ball_x = width - 2
-        else:
-            st.session_state.score_left += 1
-            st.session_state.ball_x = width // 2
-            st.session_state.ball_y = height // 2
-            st.session_state.ball_dx = -1
-            st.session_state.ball_dy = random.choice([-1, 1])
-    if st.session_state.score_left >= 5:
-        st.session_state.game_over = True
-        st.session_state.winner = "Left Player"
-    if st.session_state.score_right >= 5:
-        st.session_state.game_over = True
-        st.session_state.winner = "Right Player"
-col1, col2, col3 = st.columns([1, 2, 1])
-with col1:
-    if st.button("Up Left", key="up_left"):
-        st.session_state.paddle_left_y = max(0, st.session_state.paddle_left_y - 1)
-    if st.button("Down Left", key="down_left"):
-        st.session_state.paddle_left_y = min(height - paddle_size, st.session_state.paddle_left_y + 1)
-with col3:
-    if st.button("Up Right", key="up_right"):
-        st.session_state.paddle_right_y = max(0, st.session_state.paddle_right_y - 1)
-    if st.button("Down Right", key="down_right"):
-        st.session_state.paddle_right_y = min(height - paddle_size, st.session_state.paddle_right_y + 1)
-grid = [[" " for _ in range(width)] for _ in range(height)]
-for y in range(height):
-    grid[y][0] = "│"
-    grid[y][width - 1] = "│"
-for x in range(width):
-    grid[0][x] = "─"
-    grid[height - 1][x] =
+    st.session_state.ball_dy = random.choice([-1, 0, 1])
+if 'left_score' not in st.session_state:
+    st.session_state.left_score = 0
+if 'right_score' not in st.session_state:
+    st.session_state.right_score = 0
+if 'left_move' not in st.session_state:
+    st.session_state.left_move = 0
+
+if st.button("⬆️", key="left_up"):
+    st.session_state.left_move = -1
+if st.button("⬇️", key="left_down"):
+    st.session_state.left_move = 1
+if st.button("Restart", key="restart"):
+    st.session_state.left_score = 0
+    st.session_state.right_score = 0
+    st.session_state.left_paddle_y = (st.session_state.height - st.session_state.paddle_height) // 2
+    st.session_state.right_paddle_y = (st.session_state.height - st.session_state.paddle_height) // 2
+    st.session_state.ball_x = st.session_state.width // 2
+    st.session_state.ball_y = st.session_state.height // 2
+    st.session_state.ball_dx = random.choice([-1, 1])
+    st.session_state.ball_dy = random.choice([-1, 0, 1])
+    st.session_state.left_move = 0
+    st.rerun()
+
+new_left_y = st.session_state.left_paddle_y + st.session_state.left_move
+new_left_y = max(0, min(st.session_state.height - st.session_state.paddle_height, new_left_y))
+st.session_state.left_paddle_y = new_left_y
+st.session_state.left_move = 0
+
+if st.session_state.ball_y < st.session_state.right_paddle_y:
+    st.session_state.right_paddle_y = max(0, st.session_state.right_paddle_y - 1)
+elif st.session_state.ball_y > st.session_state.right_paddle_y + st.session_state.paddle_height - 1:
+    st.session_state.right_paddle_y = min(st.session_state.height - st.session_state.paddle_height, st.session_state.right_paddle_y + 1)
+
+st.session_state.ball_x += st.session_state.ball_dx
+st.session_state.ball_y += st.session_state.ball_dy
+
+if st.session_state.ball_y <= 0 or st.session_state.ball_y >= st.session_state.height - 1:
+    st.session_state.ball_dy *= -1
+
+if st.session_state.ball_dx < 0 and st.session_state.ball_x == 2:
+    if st.session_state.lef
