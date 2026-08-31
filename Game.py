6d30372f92b867b51c282a132d4dd5c8d994524b
siddharthkollon <siddharthkollon@gmail.com>
2026-08-31 21:51:38 +0530

    Update generated game

--- Game.py
+++ Game.py
@@ -1,28 +1,30 @@
 import streamlit as st
 from streamlit_autorefresh import st_autorefresh
 import random
-import copy
+
 st_autorefresh(interval=200, key="game_loop_ticker")
-if 'board' not in st.session_state:
-    st.session_state.board = [[0 for _ in range(10)] for _ in range(20)]
-if 'shape_id' not in st.session_state:
-    st.session_state.shape_id = 0
-if 'rotation' not in st.session_state:
-    st.session_state.rotation = 0
-if 'pos_x' not in st.session_state:
-    st.session_state.pos_x = 3
-if 'pos_y' not in st.session_state:
-    st.session_state.pos_y = 0
-if 'score' not in st.session_state:
-    st.session_state.score = 0
-if 'game_over' not in st.session_state:
-    st.session_state.game_over = False
-if 'move_left' not in st.session_state:
-    st.session_state.move_left = False
-if 'move_right' not in st.session_state:
-    st.session_state.move_right = False
-if 'rotate' not in st.session_state:
-    st.session_state.rotate = False
-if 'fast_drop' not in st.session_state:
-    st.session_state.fast_drop = False
-if 'tick' not in st.session_state:
+
+BOARD_WIDTH = 10
+BOARD_HEIGHT = 20
+EMPTY = 0
+
+SHAPES = {
+    "I": [
+        [(0, 1), (1, 1), (2, 1), (3, 1)],
+        [(2, 0), (2, 1), (2, 2), (2, 3)]
+    ],
+    "O": [
+        [(1, 0), (2, 0), (1, 1), (2, 1)]
+    ],
+    "T": [
+        [(1, 0), (0, 1), (1, 1), (2, 1)],
+        [(1, 0), (1, 1), (2, 1), (1, 2)],
+        [(0, 1), (1, 1), (2, 1), (1, 2)],
+        [(1, 0), (0, 1), (1, 1), (1, 2)]
+    ],
+    "S": [
+        [(1, 0), (2, 0), (0, 1), (1, 1)],
+        [(1, 0), (1, 1), (2, 1), (2, 2)]
+    ],
+    "Z": [
+        [(0, 0), (1, 0), (1, 1), (2,
